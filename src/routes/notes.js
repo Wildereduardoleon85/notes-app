76950bb385ba72c15ad3290cc195573b2738e1ca
Wilder León <wilder_eduardo85@hotmail.com>
2021-06-20 18:14:19 -0400

    navbar

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Note = require('../models/Note')
+const Note = require('../models/Note');
+const { isAuthenticated } = require('../helpers/auth');
 
 
-router.get('/notes/add', (req, res)=>{
+router.get('/notes/add', isAuthenticated, (req, res)=>{
     res.render('./notes/new-note')
 });
 
-router.post('/notes/new-note', async (req, res)=>{
+router.post('/notes/new-note', isAuthenticated, async (req, res)=>{
     const {titulo, descripcion} = req.body;
     const errores = [];
     if(titulo.trim() === ''){
@@ -28,24 +29,24 @@ router.post('/notes/new-note', async (req, res)=>{
     }
 });
 
-router.get('/notes', async (req, res)=>{
+router.get('/notes', isAuthenticated, async (req, res)=>{
     const notes = await Note.find().sort({fecha: 'desc'});
     res.render('./notes/all-notes', {notes});
 });
 
-router.get('/notes/edit/:id', async(req, res)=>{
+router.get('/notes/edit/:id', isAuthenticated, async(req, res)=>{
     const note = await Note.findById(req.params.id)
     res.render('./notes/edit-note', {note})
 });
 
-router.put('/notes/edit-note/:id', async(req, res)=>{
+router.put('/notes/edit-note/:id', isAuthenticated, async(req, res)=>{
     const {titulo, descripcion} = req.body;
     await Note.findByIdAndUpdate(req.params.id, {titulo, descripcion});
     req.flash('success_msg', 'Nota Actualizada!!')
     res.redirect('/notes');
 });
 
-router.delete('/notes/delete/:id', async(req, res)=>{
+router.delete('/notes/delete/:id', isAuthenticated, async(req, res)=>{
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Nota Borrada!!')
     res.redirect('/notes');
